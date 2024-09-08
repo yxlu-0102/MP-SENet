7b7fa3133b8b1ef4391311e69320520cd5d7032d
Rory Lu <yxlu0102@gmail.com>
2024-09-08 15:13:59 +0800

    initial

--- models/discriminator.py
+++ models/discriminator.py
@@ -1,12 +1,16 @@
+import sys
+sys.path.append("..")
 import torch
 import torch.nn as nn
 import numpy as np
+import torch.nn.functional as F
 from pesq import pesq
+from torch.nn.utils import spectral_norm
 from joblib import Parallel, delayed
 from utils import *
 
 
-def pesq_loss(clean, noisy, sr=16000):
+def cal_pesq(clean, noisy, sr=16000):
     try:
         pesq_score = pesq(sr, clean, noisy, 'wb')
     except:
@@ -16,12 +20,21 @@ def pesq_loss(clean, noisy, sr=16000):
 
 
 def batch_pesq(clean, noisy):
-    pesq_score = Parallel(n_jobs=15)(delayed(pesq_loss)(c, n) for c, n in zip(clean, noisy))
+    pesq_score = Parallel(n_jobs=15)(delayed(cal_pesq)(c, n) for c, n in zip(clean, noisy))
     pesq_score = np.array(pesq_score)
     if -1 in pesq_score:
         return None
     pesq_score = (pesq_score - 1) / 3.5
     return torch.FloatTensor(pesq_score)
+
+
+def metric_loss(metric_ref, metrics_gen):
+    loss = 0
+    for metric_gen in metrics_gen:
+        metric_loss = F.mse_loss(metric_ref, metric_gen.flatten())
+        loss += metric_loss
+
+    return loss
 
 
 class MetricDiscriminator(nn.Module):
@@ -46,7 +59,7 @@ class MetricDiscriminator(nn.Module):
             nn.Dropout(0.3),
             nn.PReLU(dim*4),
             nn.utils.spectral_norm(nn.Linear(dim*4, 1)),
-            LearnableSigmoid_1d(1)
+            LearnableSigmoid1d(1)
         )
 
     def forward(self, x, y):
